294e540805ff9ead0bb6b244bf9de522300868b3
GuyBlumenthal <rhindet@gmail.com>
2018-02-16 18:52:25 -0500

    Created Scouting Hub Class and Uploaded Icon
    Setup the JFrame for the scouting hub and added magnifying glass icon

---
+++ Riverdale Robotics Scouting 2018/src/scoutingapp/views/ScoutingHub.java
@@ -0,0 +1,60 @@
+package scoutingapp.views;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.GroupLayout;
+import javax.swing.GroupLayout.Alignment;
+import java.awt.Dimension;
+import java.awt.Toolkit;
+
+public class ScoutingHub extends JFrame {
+
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					ScoutingHub frame = new ScoutingHub();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public ScoutingHub() {
+		setIconImage(Toolkit.getDefaultToolkit().getImage(ScoutingHub.class.getResource("/scoutingapp/resources/MagnifyingGlass.png")));
+		setTitle("Scouting");
+		setSize(new Dimension(1024, 600));
+		setResizable(false);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 1024, 600);
+		contentPane = new JPanel();
+		contentPane.setSize(new Dimension(1024, 600));
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		GroupLayout gl_contentPane = new GroupLayout(contentPane);
+		gl_contentPane.setHorizontalGroup(
+			gl_contentPane.createParallelGroup(Alignment.LEADING)
+				.addGap(0, 422, Short.MAX_VALUE)
+		);
+		gl_contentPane.setVerticalGroup(
+			gl_contentPane.createParallelGroup(Alignment.LEADING)
+				.addGap(0, 243, Short.MAX_VALUE)
+		);
+		contentPane.setLayout(gl_contentPane);
+	}
+
+}
