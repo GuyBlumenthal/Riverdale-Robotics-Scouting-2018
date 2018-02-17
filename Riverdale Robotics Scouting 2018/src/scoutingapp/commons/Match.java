1556512f49ca684f87ec4bce5feefd8022a90f19
R3P2 <riverdaleraidersrobotics@gmail.com>
2018-02-17 10:38:47 -0500

    Created Match and Team Fields and Constructors

--- Riverdale Robotics Scouting 2018/src/scoutingapp/commons/Match.java
+++ Riverdale Robotics Scouting 2018/src/scoutingapp/commons/Match.java
@@ -1,7 +1,55 @@
 package scoutingapp.commons;
 
-public class Match {
+import java.util.ArrayList;
 
+public class Match {
 	
-	
+	int redScore, blueScore, matchID;
+
+	ArrayList<Team> teams;
+
+	int[] redSwitchPlacements;
+	int[] blueSwitchPlacements;
+
+	int[] vaultPlacements;
+
+	boolean[] climbs;
+
+	double[] redPowerUps;
+	double[] bluePowerUps;
+
+	public Match(int matchID) {
+
+		this.matchID = matchID;
+		
+		int[] redSwitchPlacements = new int[6];
+		int[] blueSwitchPlacements = new int[6];
+
+		int[] vaultPlacements = new int[6];
+
+		boolean[] climbs = new boolean[6];
+
+		for (int i = 0; i < 6; i++) {
+			
+			redSwitchPlacements[i] = -1;
+			blueSwitchPlacements[i] = -1;
+			
+			vaultPlacements[i] = -1;
+			
+			climbs[i] = false;
+			
+		}
+		
+		double[] redPowerUps = new double[3];
+		double[] bluePowerUps = new double[3];
+
+		for (int i = 0; i < 3; i++) {
+
+			redPowerUps[i] = -1;
+			bluePowerUps[i] = -1;
+
+		}
+
+	}
+
 }
