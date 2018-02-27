3f6b12d314eabe2c014e629e2fb5d8811491b7e0
R3P2 <riverdaleraidersrobotics@gmail.com>
2018-02-27 17:02:22 -0500

    Continued on converting MatchDetail information to regionalCollection

--- Riverdale Robotics Scouting 2018/src/scoutingapp/views/MatchDetail.java
+++ Riverdale Robotics Scouting 2018/src/scoutingapp/views/MatchDetail.java
@@ -18,7 +18,7 @@ import javax.swing.JTextField;
 import javax.swing.border.LineBorder;
 import javax.swing.table.DefaultTableModel;
 
-import scoutingapp.commons.ExistingTeamException;
+import scoutingapp.commons.ExistingException;
 import scoutingapp.commons.RegionalCollection;
 import scoutingapp.commons.Team;
 
@@ -30,6 +30,10 @@ public class MatchDetail extends JFrame {
 	private JLabel lblMatchNumber;
 	private JTextField txtMatchNumber;
 	private JTable tblSwitch;
+	private JTable tblScale;
+	private JTable tblVault;
+	private JTextField txtForce;
+	private JTextField txtBoost;
 
 	/**
 	 * Launch the application.
@@ -52,7 +56,7 @@ public class MatchDetail extends JFrame {
 	 */
 	public MatchDetail() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 675, 381);
+		setBounds(100, 100, 675, 406);
 		contentPane = new JPanel();
 		contentPane.setBorder(new LineBorder(new Color(0, 0, 0)));
 		setContentPane(contentPane);
@@ -94,7 +98,7 @@ public class MatchDetail extends JFrame {
 		contentPane.add(chkBasline);
 		
 		JCheckBox chkClimb = new JCheckBox("(Teleop) Climb");
-		chkClimb.setBounds(269, 88, 110, 23);
+		chkClimb.setBounds(231, 86, 110, 23);
 		contentPane.add(chkClimb);
 		
 		JSeparator separator_1 = new JSeparator();
@@ -140,33 +144,115 @@ public class MatchDetail extends JFrame {
 		scrollPaneSwitch.setViewportView(tblSwitch);
 		
 		JButton btnSaveData = new JButton("Save Data");
+		btnSaveData.setBounds(10, 333, 89, 23);
+		contentPane.add(btnSaveData);
+		
+		JLabel label = new JLabel("Scale");
+		label.setFont(new Font("Tahoma", Font.PLAIN, 12));
+		label.setBounds(231, 116, 47, 24);
+		contentPane.add(label);
+		
+		JScrollPane scrollPaneScale = new JScrollPane();
+		scrollPaneScale.setBounds(231, 140, 179, 147);
+		contentPane.add(scrollPaneScale);
+		
+		tblScale = new JTable();
+		tblScale.setModel(new DefaultTableModel(
+			new Object[][] {
+				{null, null, "0:00"},
+				{null, null, null},
+				{null, null, null},
+			},
+			new String[] {
+				"In Auto", "Defense", "Time"
+			}
+		) {
+			Class[] columnTypes = new Class[] {
+				Boolean.class, Boolean.class, String.class
+			};
+			public Class getColumnClass(int columnIndex) {
+				return columnTypes[columnIndex];
+			}
+		});
+		scrollPaneScale.setViewportView(tblScale);
+		
+		JScrollPane scrollPaneVault = new JScrollPane();
+		scrollPaneVault.setBounds(431, 140, 163, 147);
+		contentPane.add(scrollPaneVault);
+		
+		tblVault = new JTable();
+		tblVault.setModel(new DefaultTableModel(
+			new Object[][] {
+				{null, "0:00"},
+				{null, null},
+				{null, null},
+			},
+			new String[] {
+				"In Auto", "Time"
+			}
+		) {
+			Class[] columnTypes = new Class[] {
+				Boolean.class, String.class
+			};
+			public Class getColumnClass(int columnIndex) {
+				return columnTypes[columnIndex];
+			}
+		});
+		scrollPaneVault.setViewportView(tblVault);
+		
+		JLabel label_1 = new JLabel("Power Cubes In Vault");
+		label_1.setFont(new Font("Tahoma", Font.PLAIN, 12));
+		label_1.setBounds(431, 116, 129, 24);
+		contentPane.add(label_1);
+		
+		JLabel lblPower = new JLabel("Power Ups Played");
+		lblPower.setBounds(109, 298, 97, 20);
+		contentPane.add(lblPower);
+		
+		JLabel lblForce = new JLabel("Force played at: ");
+		lblForce.setBounds(109, 337, 83, 14);
+		contentPane.add(lblForce);
+		
+		txtForce = new JTextField();
+		txtForce.setBounds(192, 334, 86, 20);
+		contentPane.add(txtForce);
+		txtForce.setColumns(10);
+		
+		txtBoost = new JTextField();
+		txtBoost.setColumns(10);
+		txtBoost.setBounds(375, 334, 86, 20);
+		contentPane.add(txtBoost);
+		
+		JLabel label_2 = new JLabel("Boost played at: ");
+		label_2.setBounds(288, 337, 83, 14);
+		contentPane.add(label_2);
+		
+		JCheckBox chkLevitate = new JCheckBox("Levitate");
+		chkLevitate.setBounds(497, 333, 97, 23);
+		contentPane.add(chkLevitate);
+		
+		JCheckBox chkAlliance = new JCheckBox("Check if on Red Alliance");
+		chkAlliance.setBounds(394, 86, 166, 23);
+		contentPane.add(chkAlliance);
+		
 		btnSaveData.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseClicked(MouseEvent arg0) {
-				Team team = new Team(Integer.parseInt(txtTeamNumber.getText()));
+				Team team = new Team(Integer.parseInt(txtTeamNumber.getText()), chkAlliance.isSelected());
 				try {
-					regionalCollection.createTeam(team.getNumber());
-					//team.climb.add();
-				} catch (ExistingTeamException e) {
+					
+					regionalCollection.createTeam(team.getTeamNumber());
+					team.climb.add(chkClimb.isSelected()? 1: 0);
+					team.crossedBaseLine.add(chkBasline.isSelected()? 1: 0);
+					
+					for(int i = 0; i < tblSwitch.getRowCount() - 2; i++){
+					}
+					
+				} catch (ExistingException e) {
 					// TODO Auto-generated catch block
 					System.out.println("Team already exists");
 				}
 			}
 		});
-		btnSaveData.setBounds(10, 298, 89, 23);
-		contentPane.add(btnSaveData);
-		
-		JLabel label = new JLabel("Switch");
-		label.setFont(new Font("Tahoma", Font.PLAIN, 12));
-		label.setBounds(231, 116, 47, 24);
-		contentPane.add(label);
-		
-		JScrollPane scrollPane = new JScrollPane();
-		scrollPane.setBounds(231, 140, 147, 147);
-		contentPane.add(scrollPane);
-		
-		JScrollPane scrollPane_1 = new JScrollPane();
-		scrollPane_1.setBounds(485, 140, 147, 147);
-		contentPane.add(scrollPane_1);
 	}
 }
