3e75109870a034de04de83f647f22ac7b879e46e
William <William@10.16.116.21>
2018-03-01 15:48:29 -0500

    got rid of annoying error

--- Riverdale Robotics Scouting 2018/src/scoutingapp/views/creation/CreateMatch.java
+++ Riverdale Robotics Scouting 2018/src/scoutingapp/views/creation/CreateMatch.java
@@ -46,17 +46,17 @@ public class CreateMatch {
 	 * Launch the application.
 	 */
 	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
-					CreateMatch window = new CreateMatch();
-					window.frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
+//		EventQueue.invokeLater(new Runnable() {
+//			public void run() {
+//				try {
+//					UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+//					CreateMatch window = new CreateMatch();
+//					window.frame.setVisible(true);
+//				} catch (Exception e) {
+//					e.printStackTrace();
+//				}
+//			}
+//		});
 	}
 
 	/**
